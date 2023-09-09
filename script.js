456c1ac8cbe8454bdf4aa370dd4f93ea441b0712
Shruti Gupta <202151151@iiitvadodara.ac.in>
2023-09-09 16:35:11 +0530

    auth not working

--- script.js
+++ script.js
@@ -1,3 +1,21 @@
 const form = document.getElementById('form');
 const search = document.getElementById('search');
 const result = document.getElementById('result');
+
+async function getSongs(url="") {
+    const response = await fetch(url, {
+        method: "GET",
+        header: {
+            "Authorization": "Bearer 7D5gGhDQmR2vWaIqYoH7J9mTd8Qbu7eWtBtEMFDqhjLbLDX_2POOt_P8RapHj8er"
+        },
+        params:{
+            "q": search.value
+        }
+    });
+    const data = await response.json();
+    
+    console.log(data.artist_names);
+    
+}
+
+getSongs("https://api.genius.com/search");
